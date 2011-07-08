fb3313627ee3f252d44822eee774dd8e93e819ba
Scott Haines <scott@newfrontcreative.com>
2011-07-07 22:11:19 -0700

    Added Comments and removed useless logic and puts statements

--- source/project_euler/problem_4/problem_4.rb
+++ source/project_euler/problem_4/problem_4.rb
@@ -1,61 +1,47 @@
 #!/usr/bin/ruby
 
-@start = 100
-@cap = 999
+# PROBLEM ( PROJECT EULER #4 )
+# A palindromic number reads the same both ways. The largest palindrome made from the product of two 2-digit numbers is 9009 = 91  99.
+# Find the largest palindrome made from the product of two 3-digit numbers.
+
+@palindromic = []
 
 def calculate_numbers(start,finish,&block)
-	
 	start.upto(finish) do |n| 
 	  start.upto(finish) do |test|
 	    tmp = n*test
-	    puts "#{n.to_s}*#{test.to_s} = #{tmp.to_s}"
-	    puts tmp.to_s
-	    #(@val ||=[]) << (tmp)
 	    block.call(tmp)
 	  end
 	end
-	
 end
 
-# ["9", "9", "9", "0", "0"]
-# 99900
-# 99 9 00
-
+# Check if each number is palindromic
+# Each number is palindromic if its first half is equal to second half reversed
 def is_palindromic? num
-  length = num.to_s.length
-  half = length.to_f/2.to_f
-  
-  #puts (half % 2 == 0).to_s
-  
-  #puts 
-  #puts "#{num.to_s} length is #{length.to_s} half_size = #{half.to_s}"
-  # check first half, second half (reversed) if num/2 % 2
-  #puts (length.to_f / 2.to_f).to_s
-  
-  # if number is 2 digits
-  if length == 2
-    #puts "Number is 2 digit"
-    return num if num[0] == num[1]
-    
-  else
-   # puts "Number divided by 2 is even number"
-    num_as_array = num.to_s.scan(/./)
-    #puts num_as_array.inspect
-    first_half = num_as_array.join[0,half]
-    second_half = num_as_array.join.reverse[0,half]
-    #puts "first half: #{first_half} | second half: #{second_half}"
-    return num if first_half == second_half
-    #puts
+  begin
+    length = num.to_s.length
+    half = length.to_f/2.to_f
+    if length == 2
+      return num if num[0] == num[1]
+    else
+      num_as_array = num.to_s.scan(/./)
+      first_half = num_as_array.join[0,half]
+      second_half = num_as_array.join.reverse[0,half]
+      return num if first_half == second_half
+    end
+  rescue
+    puts "error on block...."
   end
-  #puts
-    
 end
 
-@palindromic = []
-calculate_numbers(@start,@cap) do |n|
+# calculate each number in the range against itself against the range
+#  push number from each result to block
+calculate_numbers(100,999) do |n|
+  # push palindrom to set, ensure one of each number, should only be one...
   if (is_palindromic? n) && (!@palindromic.include? n)
     @palindromic << n
   end
 end
 
+# show results, sorted from smallest to largest
 puts @palindromic.sort.inspect
