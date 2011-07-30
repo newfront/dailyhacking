590bc21e288cbdd70b7459021cd415680952f595
Scott Haines <scott@newfrontcreative.com>
2011-07-30 11:36:27 -0700

    added notes

--- source/algorithms/sorted_merge.rb
+++ source/algorithms/sorted_merge.rb
@@ -23,21 +23,20 @@ def merge(list,list2)
 	  if list[0] > list2[0]
 	    puts "#{list[0].to_s} > #{list2[0].to_s}"
 	    @result << list2[0]
-	    list2.slice!(0) if list2.size > 1
+	    list2.slice!(0) if list2.size > 1 # remove smallest element
 	    #@result << list[0]
 	  elsif list[0] < list2[0]
       puts "#{list[0].to_s} < #{list2[0].to_s}"
 	    @result << list[0]
-	    list.slice!(0) if list.size > 1
+	    list.slice!(0) if list.size > 1 # remove smallest element
 	    #@result << list2[0]
 	  else
 	    # results are the same
 	    @result << list[0] # don't worry about the second, it is a repeat
-	    list2.slice!(0) if list2.size > 1
-	    list.slice!(0) if list.size > 1
+	    list2.slice!(0) if list2.size > 1 # remove compared elements
+	    list.slice!(0) if list.size > 1 # remove compared elements
 	  end
 	  
-	  # remove compared elements
     # recursively run until list.size || list2.size == 1
     puts list.inspect
     puts list2.inspect
