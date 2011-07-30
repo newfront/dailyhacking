07f80a7af51b9c0618edef3c84b57d5279b9b597
Scott Haines <scott@newfrontcreative.com>
2011-07-30 11:26:18 -0700

    Adding sorted_list merging algorithm

---
+++ source/algorithms/sorted_merge.rb
@@ -0,0 +1,49 @@
+#!/usr/bin/env ruby
+
+@list = [1,2,5,7,9,25]
+@list2 = [4,6,8,11,29]
+@result = []
+
+def merge(list,list2)
+
+	if list.size == 1 && list2.size == 1
+		if list[0] > list2[0]
+		  @result << list2[0]
+		  @result << list[0]
+		elsif list[0] < list2[0]
+		  @result << list[0]
+		  @result << list2[0]
+		else
+		  @result << list[0]
+		  #@result << list2[0] // same value
+		end
+		return @result
+	else
+	  # compare first element of either list to see what is bigger
+	  if list[0] > list2[0]
+	    puts "#{list[0].to_s} > #{list2[0].to_s}"
+	    @result << list2[0]
+	    list2.slice!(0) if list2.size > 1
+	    #@result << list[0]
+	  elsif list[0] < list2[0]
+      puts "#{list[0].to_s} < #{list2[0].to_s}"
+	    @result << list[0]
+	    list.slice!(0) if list.size > 1
+	    #@result << list2[0]
+	  else
+	    # results are the same
+	    @result << list[0] # don't worry about the second, it is a repeat
+	  end
+	  
+	  # remove compared elements
+    # recursively run until list.size || list2.size == 1
+    puts list.inspect
+    puts list2.inspect
+    merge(list,list2)
+	end
+
+end
+
+puts "test merging lists"
+merged_list = merge(@list,@list2)
+puts merged_list.inspect
