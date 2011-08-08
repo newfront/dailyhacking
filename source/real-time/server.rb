0b16eb1b8af75c46163dd1e4aaf918b4c22f1921
Scott Haines <scott@newfrontcreative.com>
2011-08-07 23:45:58 -0700

    fixed a few other bugs. narrowing down text and search words

--- source/real-time/server.rb
+++ source/real-time/server.rb
@@ -18,40 +18,195 @@ require 'yajl'
 require 'yaml'
 require 'pp'
 
-# connect to mongodb
-# store each tweet into a queue
-# grab words from each tweet, generate word count
+# Google Image Search API 
+@google_dev_key = "ABQIAAAAqj2FkdNbZi_ZKy1fY_HdjxQsPFBHlSdy9MSZLoC4ErdaOPqPGhSsI3FcfI7uPySPy7zgD5eo88rAWw"
+@google_dev_url = "http://gravitateapp.com"
 
-
-@follow_twitter_handles = ["@aol","@aoltravel","@editions"]
-
-#["place", nil]
-#["text", "Tips for traveling (or not) with the iPad: \n\t\n\n\tGary Arndt has written an interesting blog post about traveling ... http://aol.it/ooqRJj"]
-#["favorited", false]
-#["truncated", false]
-#["in_reply_to_status_id_str", nil]
-#["created_at", "Sun Aug 07 22:37:22 +0000 2011"]
-#["geo", nil]
-#["in_reply_to_screen_name", nil]
-#["in_reply_to_user_id", nil]
-#["source", "<a href=\"http://twitterfeed.com\" rel=\"nofollow\">twitterfeed</a>"]
-#["in_reply_to_user_id_str", nil]
-#["in_reply_to_status_id", nil]
-#["contributors", nil]
-#["retweeted", false]
-#["coordinates", nil]
-#["activities", {"favoriters"=>[], "favoriters_count"=>"0", "repliers"=>[], "repliers_count"=>"0", "retweeters_count"=>"0", "retweeters"=>[]}]
-#["user", {"favourites_count"=>34, "location"=>"55.921999,-4.447519", "follow_request_sent"=>nil, "contributors_enabled"=>false, "profile_link_color"=>"0084B4", "followers_count"=>302, "following"=>nil, "profile_sidebar_border_color"=>"C0DEED", "time_zone"=>"Edinburgh", "created_at"=>"Thu Jul 19 10:15:09 +0000 2007", "friends_count"=>551, "description"=>"Enterprise Security Architect, technology enthusiast, Apple geek, amateur photographer, father of 2, patience of a saint!", "is_translator"=>false, "profile_use_background_image"=>true, "default_profile"=>true, "listed_count"=>11, "profile_background_color"=>"C0DEED", "show_all_inline_media"=>false, "geo_enabled"=>true, "profile_background_image_url_https"=>"https://si0.twimg.com/images/themes/theme1/bg.png", "profile_background_image_url"=>"http://a0.twimg.com/images/themes/theme1/bg.png", "protected"=>false, "verified"=>false, "profile_text_color"=>"333333", "url"=>"http://alexwaddell.co.uk", "profile_image_url"=>"http://a1.twimg.com/profile_images/482004362/IMG_0608_normal.jpg", "name"=>"Alex Waddell", "default_profile_image"=>false, "statuses_count"=>2932, "notifications"=>nil, "profile_sidebar_fill_color"=>"DDEEF6", "profile_image_url_https"=>"https://si0.twimg.com/profile_images/482004362/IMG_0608_normal.jpg", "id"=>7582812, "id_str"=>"7582812", "lang"=>"en", "profile_background_tile"=>false, "utc_offset"=>0, "screen_name"=>"alexwaddell"}]
-#["retweet_count", 0]
-#["id", 100334747837931521]
-#["possibly_sensitive", false]
-#["id_str", "100334747837931521"]
-#["entities", {"urls"=>[{"indices"=>[116, 136], "url"=>"http://aol.it/ooqRJj", "expanded_url"=>nil}], "user_mentions"=>[], "hashtags"=>[]}]
-
-@most_common_words = ['the','of','to','and','a','in','is','it','you','that','he','was','for','on','are','with','as','I','his','they','be','at','one','have','this','from','or','had','by','not','but','some','what','there','we','can','out','other','were','all','your','when','up','use','word','how','said','an','each','she','which','do','their','time','if','will','way','about','many','then','them','would','write','like','so','these','her','long','make','thing','see','him','two','has','look','more','day','could','go','come','did','my','sound','no','most','number','who','over','water','than','call','first','may','down','side','been','now','find','any','new','part','take','get','place','made','live','where','after','back','little','only','round','man','year','came','show','every','good','me','give','our','under']
-@common_words = ['stop','few','life','real','night','close','press','while','run','late','left','draw','sea','far','saw','story','might','start','hard','slice','cross','tree','city','between','door','last','never','eye','keep','let','thought','four','sun','food','cover','plant','learn','still','study','grow','school','answer','found','country','should','page','own','stand','head','father','earth','self','build','near','world','mother','point','animal','again','us','try','picture','house','need','off','kind','light','went','change','men','ask','why','act','such','high','big','must','here','land','even','add','spell','large','port','hand','read','home','put','end','small','play','also','well','air','want','three','set','sentence','tell','does','too','old','boy','right','move','differ','mean','same','cause','turn','before','line','low','help','say','think','great','much','form','just','through','very','name']
+@search_words = ['AOL','Huffington Post','Tech Crunch','Joystiq']
+@most_common_words = ['the','because','of','to','and','a','in','is','it','you','that','he','was','for','on','are','with','as','I','his','they','be','at','one','have','this','from','or','had','by','not','but','some','what','there','we','can','out','other','were','all','your','when','up','use','word','how','said','an','each','she','which','do','their','time','if','will','way','about','many','then','them','would','write','like','so','these','her','long','make','thing','see','him','two','has','look','more','day','could','go','come','did','my','sound','no','most','number','who','over','water','than','call','first','may','down','side','been','now','find','any','new','part','take','get','place','made','live','where','after','back','little','only','round','man','year','came','show','every','good','me','give','our','under']
+@common_words = ['stop','few','life','real','night','close','press','while','run','late','left','draw','sea','far','saw','story','might','start','hard','slice','cross','tree','city','between','door','last','never','eye','keep','let','thought','four','sun','food','cover','plant','learn','still','study','grow','school','answer','found','country','should','page','own','stand','head','father','earth','self','build','near','world','mother','point','animal','again','us','try','picture','house','need','off','kind','light','went','change','men','ask','why','act','such','high','big','must','here','land','even','add','spell','large','port','hand','read','home','put','end','small','play','also','well','air','want','three','set','sentence','tell','does','too','old','boy','right','move','differ','mean','same','cause','turn','before','line','low','help','say','think','great','much','form','just','through','very','name','liked','hello']
 @less_common_words = ['open','seem','together','next','white','children','begin','got','walk','example','ease','paper','often','always','music','those','both','mark','book','letter','until','mile','river','car','feet','care','second','group','carry','took','rain','eat','room','friend','began','idea','fish','mountain','north','once','base','hear','horse','cut','sure','watch','color','face','wood','main','enough','plain','girl','usual','young','ready','above','ever','red','list','though','feel','talk','bird','soon','body','dog','family','direct','pose','leave','song','measure','state','product','black','short','numeral','class','wind','question','happen','complete','ship','area','half','rock','order','fire','south','problem','piece','told','knew','pass','farm','top','whole','king','size','heard','best','hour','better','true','during','hundred','am','remember','step','early','hold','west','ground','interest','reach','fast','five','sing','listen','six','table','travel','less','morning']
-@lesser_common_words = ['ten','simple','several','vowel','toward','war','lay','against','pattern','slow','center','love','person','money','serve','appear','road','map','science','rule','govern','pull','cold','notice','voice','fall','power','town','fine','certain','fly','unit','lead','cry','dark','machine','note','wait','plan','figure','star','box','noun','field','rest','correct','able','pound','done','beauty','drive','stood','contain','front','teach','week','final','gave','green','oh','quick','develop','sleep','warm','free','minute','strong','special','mind','behind','clear','tail','produce','fact','street','inch','lot','nothing','course','stay','wheel','full','force','blue','object','decide','surface','deep','moon','island','foot','yet','busy','test','record','boat','common','gold','possible','plane','age','dry','wonder','laugh','thousand','ago','ran','check','game','shape','yes','hot','miss','brought','heat','snow','bed','bring','sit','perhaps','fill','east','weight','language','among']
+@lesser_common_words = ['ten','simple','several','vowel','toward','war','lay','against','pattern','slow','center','love','person','money','serve','appear','road','map','science','rule','govern','pull','cold','notice','voice','fall','power','town','fine','certain','fly','unit','lead','cry','dark','machine','note','wait','plan','figure','star','box','noun','field','rest','correct','able','pound','done','beauty','drive','stood','contain','front','teach','week','final','gave','green','oh','quick','develop','sleep','warm','free','minute','strong','special','mind','behind','clear','tail','produce','fact','street','inch','lot','nothing','course','stay','wheel','full','force','blue','object','decide','surface','deep','moon','island','foot','yet','busy','test','record','boat','common','gold','possible','plane','age','dry','wonder','laugh','thousand','ago','ran','check','game','shape','yes','hot','brought','heat','snow','bed','bring','sit','perhaps','fill','east','weight','language','among']
+
+@words = {}
+@users = {}
+@urls = {}
+
+def parse_tweet(tweet)
+  
+  tmp_words = {}
+  tmp_urls = {}
+  tmp_users = {}
+  
+  # does it start with RT?
+  retweet = false
+  if tweet.match(/^(RT)/i)
+    tweet = tweet.gsub(/^(RT )/i,"")
+    retweet = true
+  end 
+  
+  # grab words in quotes
+  user_quoted_text = []
+  matches = tweet.match(/(["|'](.*)['|"])/)
+  if matches 
+    #puts "#{matches[2].inspect}"
+    user_quoted_text << matches[2] 
+  end
+  puts "User enclosed this in Quotes: #{user_quoted_text[0]}" if !user_quoted_text.empty?
+  
+  #@search_words.each {|s_word|
+  #  tweet = tweet.gsub(s_word.downcase,"")
+  #  tweet = tweet.gsub(s_word.upcase,"")
+  #  tweet = tweet.gsub(s_word,"")
+  #}
+  
+  # split words in tweet on space
+  words = tweet.split(/\s/)
+  
+  # check important words, return hash
+  importants = view_important(words)
+  
+  words.each{|word|
+    # check if the word is a twitter username
+    if word.match(/@[A-Za-z0-9_]*/)
+      #puts "**************************"
+      #puts "\nFound Twitter User: #{word.to_s}\n"
+      # store the twitter user in a hash with init count, unless already in hash
+      
+      # store users in temp
+      unless tmp_users.has_key?(word)
+        tmp_users.store(word,1)
+      else
+        tmp_users[word] = (tmp_users[word] += 1)
+      end
+      
+      unless @users.has_key?(word)
+        @users.store(word,1)
+      else
+        count = @users[word]
+        count += 1
+        #puts "Count is now #{count.to_s}"
+        @users[word] = count
+        #puts @users[word].inspect
+      end 
+      #puts "*************************"
+    elsif word.match(/http:.*/i)
+      #puts "***************************"
+      #puts "URL: #{word.to_s}"
+      
+      # store temp url
+      unless tmp_urls.has_key?(word)
+        tmp_urls.store(word,1)
+      else
+        tmp_urls[word] = (tmp_urls[word] += 1)
+      end
+      
+      unless @urls.has_key?(word)
+        @urls.store(word,1)
+      else
+        @urls[word] = (@urls[word] += 1)
+      end
+      
+      #puts "***************************"
+    else
+      
+      #store local words
+      unless tmp_words.has_key?(word)
+        tmp_words.store(word,1)
+      else
+        tmp_words[word] = (tmp_words[word] += 1)
+      end
+      
+      unless @words.has_key?(word)
+        @words.store(word,1) unless word.length <= 1
+      else
+        count = @words[word]
+        count += 1
+        @words[word] = count
+        #puts @words[word].inspect
+      end
+      
+    end
+    
+  }
+  
+  # return parsed tweet
+  tweet = {:words => tmp_words, :users => tmp_users, :urls => tmp_urls, :retweet => retweet}
+  
+  tweet = tweet.merge(importants)
+  
+  tmp_words = nil
+  tmp_users = nil
+  tmp_urls = nil
+  
+  return tweet
+  
+end
+
+def view_important(list)
+  importants = []
+  called_out = []
+  
+  list.each{|word|
+    
+    unless word.length < 2
+      #word = word.downcase
+      block = false
+      # check if word ends in exclimation point
+      
+      if word.match(/([A-Za-z]*)!/)
+        puts "Word ends with exclimation point. Seems interesting #{word.to_s}"
+        called_out << word unless called_out.include? word
+      end
+      
+      # test against users
+      if word.match(/@[A-Za-z0-9_]*/)
+        #puts "just a user"
+        block = true
+      end
+      
+      # check if its a URL
+      if word.match(/http:.*/i)
+        #puts "just a url"
+        block = true
+      end
+      
+      # check against 4 common word lists
+      # @most_common_words
+      # @common_words
+      # @less_common_words
+      # @lesser_common_words
+      unless block
+        if @most_common_words.include? word.downcase
+          #puts "Word is most common: #{word.to_s}"
+        elsif @common_words.include? word.downcase
+          #puts "Word is common: #{word.to_s}"
+        elsif @less_common_words.include? word.downcase
+          #puts "Word is less common #{word.to_s}"
+        elsif @lesser_common_words.include? word.downcase
+          #puts "Word is least common: #{word.to_s}"
+        elsif word == word.upcase
+          called_out << word unless called_out.include? word
+        else
+          #puts "Word seems kind of important: #{word.to_s}"
+          importants << word unless importants.include? word
+        end
+      end
+    end
+  }
+  
+  puts "Important Words from Tweet: #{importants.inspect}"
+  
+  # notion that important words will make good image (first few....)
+  # google image seach api
+  
+  puts "Search For: #{importants[0..1].join(" ")}"
+  
+  puts "Called Out from Tweet: #{called_out.inspect}"
+  
+  return {:i_words => importants, :search => importants[0..1].join(" "), :call_outs => called_out}
+  
+end
 
 def tweet_reduce
   
@@ -72,13 +227,13 @@ def count_words(tweet)
 end
 
 
-def handle_tweet(tweet)
+def grab_tweet(tweet)
   
   puts "Rate Limit Error: " if tweet == "Easy there, Turbo. Too many requests recently. Enhance your calm."
   
   begin
     tweet = JSON.parse(tweet)
-    pp tweet
+    #pp tweet
   rescue JSON::ParserError => e
     puts "message: #{tweet.to_s}"
     puts "Don't parse non JSON. Wasted your time: #{e.inspect}"
@@ -87,20 +242,27 @@ def handle_tweet(tweet)
   return unless tweet['text']
   
   #pp tweet
+  tmp_tweet = parse_tweet(tweet['text'])
+  
+  puts "Parse Results: #{tmp_tweet.inspect}"
+  
   puts "====================================="
   puts 
   puts "@#{tweet['user']['screen_name']}: #{tweet['text']}"
   puts "Hashtags: #{tweet['entities']['hashtags'].inspect}"
+  puts
   puts "====================================="
+  
 end
 
+# open up stream to Twitter (pipe)
 def stream_request
-  puts "=================="
-  puts "trying to grab info from stream"
+  puts "==================================\n\n"
+  puts "Start Grabing Info from the Stream"
   
   @request_url = 'http://stream.twitter.com/1/statuses/filter.json'
   #@request_url = 'http://stream.twitter.com/1/statuses/sample.json'
-  http = EventMachine::HttpRequest.new(@request_url).post :head => {'Authorization'=> ["newfront","Turtlepizza"],'Content-Type'=>"application/x-www-form-urlencoded"}, :body => "track=AOL,editions,ipad,america,online,techcrunch,HuffingtonPost"
+  http = EventMachine::HttpRequest.new(@request_url).post :head => {'Authorization'=> ["newfront","Turtlepizza"],'Content-Type'=>"application/x-www-form-urlencoded"}, :body => "track=AOL,Editions by AOL,america online,aim chat,webaim,joystiq"
   #puts http.inspect
   buffer = ""
   
@@ -108,14 +270,16 @@ def stream_request
     
     buffer += chunk
     while line = buffer.slice!(/.+\r?\n/)
-      handle_tweet(line)
+      grab_tweet(line)
     end
   end
   
-  #http.errback {
-  #  puts "error on that one. try again"
-  #  stream_request
-  #}
+  http.errback {
+    puts "error on that one. try again"
+    EventMachine::Timer.new(5) do
+      stream_request
+    end
+  }
   
 end
 
