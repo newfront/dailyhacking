05026e0b12de21b517f2ec8afab9a9cd0229f426
Scott Haines <scott@newfrontcreative.com>
2012-04-14 20:41:57 -0700

    Updated to allow an element to be removed from Single Linked List

--- trees_lists/linked_lists/java/single_linked_lists/src/com/newfrontcreative/linkedlists/ListItem.java
+++ trees_lists/linked_lists/java/single_linked_lists/src/com/newfrontcreative/linkedlists/ListItem.java
@@ -5,32 +5,32 @@ public class ListItem {
 	 * Instance Variables
 	 */
 	public int data;
-    public ListItem next;
+	public ListItem next;
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
 		System.out.println("here we go");
 	}
-	
+
 	public ListItem(int dData)
-    {
-        data = dData;
-    }
-    
-    public void setNext(ListItem item)
-    {
-        next = item;
-    }
-    
-    public ListItem getNext()
-    {
-    	return next;
-    }
-    
-    public int getData()
-    {
-        return data;
-    }
+	{
+		data = dData;
+	}
+
+	public void setNext(ListItem item)
+	{
+		next = item;
+	}
+
+	public ListItem getNext()
+	{
+		return next;
+	}
+
+	public int getData()
+	{
+		return data;
+	}
 
 }
