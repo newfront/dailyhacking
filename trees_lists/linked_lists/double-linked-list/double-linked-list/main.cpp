af4a9639a7a48d5fb536a3b5f66737236b8047a9
Scott Haines <scott@newfrontcreative.com>
2012-04-14 20:03:41 -0700

    Java Implementation for Single Linked Lists - with min, max, pop, shift, insertAtFront, insertAtEnd, as well as some iterable methods

---
+++ trees_lists/linked_lists/double-linked-list/double-linked-list/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  double-linked-list
+//
+//  Created by Scott Haines on 4/14/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#include <iostream>
+
+int main (int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
---
+++ trees_lists/linked_lists/java/single_linked_lists/src/com/newfrontcreative/linkedlists/ListItem.java
@@ -0,0 +1,36 @@
+package com.newfrontcreative.linkedlists;
+
+public class ListItem {
+	/**
+	 * Instance Variables
+	 */
+	public int data;
+    public ListItem next;
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("here we go");
+	}
+	
+	public ListItem(int dData)
+    {
+        data = dData;
+    }
+    
+    public void setNext(ListItem item)
+    {
+        next = item;
+    }
+    
+    public ListItem getNext()
+    {
+    	return next;
+    }
+    
+    public int getData()
+    {
+        return data;
+    }
+
+}
